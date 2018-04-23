fb1dee58fba165adc535fa17c9b4ecd17e4fa717
Developer Genius <38667043+developergenius@users.noreply.github.com>
2018-04-23 13:53:22 -0400

    Add files via upload

---
+++ TIME_QUOTE2.py
@@ -0,0 +1,13 @@
+square_ft =  (input(' How many bed rooms?  ')
+              
+while True:
+    if:
+        square_ft == 1
+        square_ft = Square_ft + 575)
+    elif:
+        square_ft == 2
+        square_ft = square_ft + 1007)
+    else:
+        break
+
+              
